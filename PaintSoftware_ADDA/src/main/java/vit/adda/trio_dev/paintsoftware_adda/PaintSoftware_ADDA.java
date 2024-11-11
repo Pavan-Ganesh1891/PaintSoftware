51a3b8ea138c07858ac555731202737af7af20ae
PavanGanesh1891 <malyadri.pavanganesh2021@vitstudent.ac.in>
2024-11-11 21:32:14 +0530

    Initial commit

---
+++ PaintSoftware_ADDA/src/main/java/vit/adda/trio_dev/paintsoftware_adda/PaintSoftware_ADDA.java
@@ -0,0 +1,8 @@
+package vit.adda.trio_dev.paintsoftware_adda;
+
+public class PaintSoftware_ADDA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
