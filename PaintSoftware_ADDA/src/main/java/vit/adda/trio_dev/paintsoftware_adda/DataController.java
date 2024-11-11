4abea5488a61072a1fe930582e4084da40a25348
Developer D2 <d2@gmail.com>
2024-11-11 22:36:17 +0530

    D2: Work done

---
+++ PaintSoftware_ADDA/src/main/java/vit/adda/trio_dev/paintsoftware_adda/DataController.java
@@ -0,0 +1,7 @@
+package vit.adda.trio_dev.paintsoftware_adda;
+
+public class DataController {
+    public void controlData() {
+        System.out.println("Controlling data.");
+    }
+}
---
+++ PaintSoftware_ADDA/src/main/java/vit/adda/trio_dev/paintsoftware_adda/DrawingContext.java
@@ -0,0 +1,21 @@
+package vit.adda.trio_dev.paintsoftware_adda;
+
+public class DrawingContext {
+    public String setPoint() {
+        System.out.println("Point set on the screen.");
+        return "Point set on the screen.";
+    }
+
+    public void clearScreen() {
+        System.out.println("Screen cleared.");
+    }
+
+    public void getVerticalSize() {
+        System.out.println("Vertical size of window is 1080");
+    }
+
+    public void getHorizontalSize() {
+        System.out.println("Horizontal size of window is 1920.");
+    }
+}
+
